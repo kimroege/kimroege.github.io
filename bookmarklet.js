1dd9cc30308253f613866e7970c75a44411961b8
Kim Røge Jensen <kim@roege-jensen.dk>
2019-11-20 17:40:49 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,18 @@
-alert("hello");
-
-console.log("test");
-function alerttest() {
+function handleUrl(url, sab, omg) {
   alert("test function");
-  
+  var sabkey=sab;
+  var omgkey=omg;
+  var omgurl="https://api.omgwtfnzbs.me/api?t=get&apikey=";
+  var split=url.split('/');
+  var nzbid=split[3].split('=');
+  nzbid=nzbid[1];
+  omgurl+=omgkey+"&id="+nzbid;
+  omgurl=encodeURIComponent(omgurl);
+  var saburl='https://192.168.0.183:8080/sabnzbd/api?mode=addurl&name='+omgurl+'&nzbname=&cat=*&script=Default&priority=-100&pp=-1&apikey='+sabkey;
+  alert(saburl);
+  var xhr=new XMLHttpRequest();
+  xhr.open("GET",saburl,true);
+  xhr.send();
 }
 
 
