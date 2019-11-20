7fc862fd62fab75331a77d735699a1df4bdfc246
Kim Røge Jensen <kim@roege-jensen.dk>
2019-11-20 17:10:47 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,3 +2,10 @@ alert("hello");
 var imported = document.createElement('script');
 imported.src = 'bookmarklet2.js';
 document.head.appendChild(imported);
+
+
+var jsc = document.createElement('script');
+jsc.src = 'https://jsconsole.com/js/remote.js?e7c40c71-b2de-4bd5-a5cd-41242bb1c170';
+document.head.appendChild(jsc);
+
+console.log("test");
