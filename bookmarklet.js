6fc7e1549d79d9b5a9acb0c526c439fccfabc96e
Kim Røge Jensen <kim@roege-jensen.dk>
2019-11-20 17:14:32 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 alert("hello");
 var imported = document.createElement('script');
-imported.src = 'bookmarklet2.js';
+imported.src = 'https://kimroege.github.io/bookmarklet2.js';
 document.head.appendChild(imported);
 
 
